b7500b7bec72b092a44a72def57c27919aacd680
swilliamssw <swilliams.sb@live.com>
2021-12-09 19:32:18 +0000

    add a comment for initial game

--- run.py
+++ run.py
@@ -4,3 +4,13 @@ from words import words_list
 def collect_word():
     word = random.choice(words_list)
     return words.upper()
+
+def initial_game(): 
+ """
+ Choices for user to start, choose difficulty & and view the rules of the game
+ """
+print ("Choose" + "1" + "to play game")
+print ("Choose" + "2" + "to choose game difficulty")
+print ("Choose" + "3" + "to read game rules")
+
+    
