7bc2fac1dc017edeab919024b80a1c95dc46a3c6
swilliamssw <swilliams.sb@live.com>
2021-12-10 05:00:14 +0000

    write out logical steps to write code to

--- run.py
+++ run.py
@@ -13,3 +13,23 @@ print(words_list)
 print("Choose" + "1" + "to play game")
 print("Choose" + "2" + "to choose game difficulty")
 print("Choose" + "3" + "to read game rules")
+
+
+
+#Logic 
+
+# 1. Display menu 
+
+# 2. handle choice
+# 3. get random word
+# 4. display 'hidden word' e.g _ _ _ _ _ _ 
+# 5. let user guess 
+# 6. validate tht e guess, MUST be a letter value
+# 7. check if guessed letter has already been guessed
+# 8. check secret word to see if letter appears 
+# 9a. if true then reveal letters in 'hidden word'
+# 9b. else lose a life and hang the man 
+# 10a. check if the word has been guessed fully 
+# 10b. if true win, else lose if no lives are left 
+# 11. keep guessing and repeat until 10a
+# 12. finish game allow the player to restart the game
