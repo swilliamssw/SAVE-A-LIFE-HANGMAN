db115d00827ea7b9f0b39c4b0a6c807dd4762828
swilliamssw <swilliams.sb@live.com>
2021-12-11 05:28:54 +0000

    add a start game function with variables that outline the guessed letter and number of lifes of the player, also add a while loop which will loop until the word is either guessed or the player run out of lives/tries

--- run.py
+++ run.py
@@ -1,35 +1,66 @@
 import random
 from words import words_list
 
-def collect_word():
-    word = random.choice(words_list)
-    return word.upper()
+# 1. Display menu
 
-def game_menu(): 
- """
- Choices for user to start, choose difficulty & and view the rules of the game
- """
-print(words_list)
-print("Choose" + "1" + "to play game")
-print("Choose" + "2" + "to choose game difficulty")
-print("Choose" + "3" + "to read game rules")
+def game_menu():
+    """
+    Choices for user to start, choose difficulty & and view the rules of the game
+    """
+print("Choose 1 to play game")
+print("Choose 2 to choose game difficulty")
+print("Choose 3 to read game rules")
+choices = False
+while not choices:
+    options = input("/n ")
+    if options == "1":
+        choices = True
+        difficulty = "default"
+        return difficulty
 
+    elif options == "2":
+        choices = True
 
+    elif options == "3":
+        choices = True
 
-#Logic 
-
-# 1. Display menu 
+    else: print("Please choose 1, 2 or 3 to continue")
 
 # 2. handle choice
+
 # 3. get random word
-# 4. display 'hidden word' e.g _ _ _ _ _ _ 
-# 5. let user guess 
-# 6. validate tht e guess, M§AUST be a letter value
+def collect_word(words_list):
+    random_word = random.choice(words_list)
+    while '_' in random_word or ' ' in random_word:
+    return random_word.upper()
+
+# 4. display 'hidden word' e.g _ _ _ _ _ _
+def Start_game(collect_word):
+    correct_word = " _ " * len(collect_word)
+    guessed_letters = []
+    lives = 7
+    guessed = False
+    print("Save a man's life by guessing the correct letters to complete the word!")
+    print(correct_word)
+    print("/n")
+    while not guessed and live > 0:
+        guess = input ("Please guess a letter: ")
+    if len(guess) == 1 and guess.isalpha():
+        
+    elif len(guess) == len(correct_word) and guess.alpha():
+
+    else: print("Sorry that is not a valid input, please guess a letter from A - Z")
+
+
+        
+
+# 5. let user guess
+# 6. validate the guess, MUST be a letter value
 # 7. check if guessed letter has already been guessed
-# 8. check secret word to see if letter appears 
+# 8. check secret word to see if letter appears
 # 9a. if true then reveal letters in 'hidden word'
-# 9b. else lose a life and hang the man 
-# 10a. check if the word has been guessed fully 
-# 10b. if true win, else lose if no lives are left 
+# 9b. else lose a life and hang the man
+# 10a. check if the word has been guessed fully
+# 10b. if true win, else lose if no lives are left
 # 11. keep guessing and repeat until 10a
 # 12. finish game allow the player to restart the game
