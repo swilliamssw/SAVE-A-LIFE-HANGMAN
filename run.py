046420d65d6b74e631a93f08a17bd163dbf5bf37
swilliamssw <swilliams.sb@live.com>
2021-12-12 10:49:40 +0000

    add hangman visuals and add for winning and losing the game

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ def game_menu():
     Choices for user to start, 
     choose difficulty & view the rules of the game.
     """
-
+    """
 print("Choose 1 to play game")
 print("Choose 2 to choose game difficulty")
 print("Choose 3 to read game rules")
@@ -20,7 +20,6 @@ while not choices:
     if options == "1":
         choices = True
         difficulty = "default"
-        return difficulty
 
     elif options == "2":
         choices = True
@@ -30,7 +29,7 @@ while not choices:
 
     else:
         print("Please choose 1, 2 or 3 to continue")
-
+"""
 # 2. handle choice
 
 # 3. get random word
@@ -52,42 +51,138 @@ def Start_game(collect_word):
     guessed = False
     print("Save a man's life by guessing the correct letters to complete the word!")
     print(correct_word)
-    print("/n")
-   
+    
+
     while not guessed and lives > 0:
         player_guess = input("Please guess a letter: ")
+
         if len(player_guess) == 1 and player_guess.isalpha():
+
             if (player_guess) in guessed_letters:
                 print("That letter has already been guessed, please try another", player_guess)
+
             elif player_guess not in collect_word:
                  print(player_guess, "is not a valid word.")
                  lives -= 1
                  guessed_letters.append(player_guess)
+
             else:
                 print("Great guess!", player_guess, "is one of the letters")
                 guessed_letters.append(player_guess)
                 list_of_words = list(correct_word)
                 indices = [i for i, letter in enumerate(correct_word) if letter == player_guess]
+
                 for index in indices:
                    word_temp_list[index] = player_guess
-                word_temp = "".join(word_temp_list)  
+                word_temp = "".join(word_temp_list) 
+                
             if "_" not in word_temp:
                 guessed = True    
+
         elif len(player_guess) == len(correct_word) and player_guess.alpha():
-           if player_guess in guessed_word:
-              print("You already guessed", player_guess)
-           elif player_guess != correct_word:
+
+            if player_guess in guessed_word:
+                print("You already guessed", player_guess)
+
+            elif player_guess != correct_word:
                 print(player_guess, "Is not the word")
                 lives -= 1 
                 guessed_word.append(player_guess)
-           else: 
+
+            else: 
                 guessed = True 
                 completed_word = correct_word
+
+
         else:
             Print("Sorry that is not a valid input, please guess a letter from A-Z ")
-        Print(display_hangman(lives))
+        Print(hangman_display(lives))
         print(completed_word)
         print("\n")
+    
+    if guessed:
+        print("CONGRATULATIONS!")
+        print("YOU WIN!")
+        print("YOU SAVED A LIFE TODAY!")
+    else:
+        print("OH NO!")
+        print("It's a very unfortante day, you was unable to save the man this time")
+        print("The correct word was" + correct_word + "Maybe next time you will try harder :(")
+
+def hangman_display(lives):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                   --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[lives]
+
 # 5. let user guess
 # 6. validate the guess, MUST be a letter value
 # 7. check if guessed letter has already been guessed
